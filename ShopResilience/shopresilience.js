b738f99bebc8de1fb69bfe3df224cf2c9b8577b9
carberryrob <rcarberry@strategicfactory.com>
2023-08-24 00:26:15 -0400

    Changes to be committed: 	modified:   ShopResilience/shopresilience.js

--- ShopResilience/shopresilience.js
+++ ShopResilience/shopresilience.js
@@ -12,99 +12,63 @@
 
 <script>
 
-function getDSTDates(startYear, endYear) {
+function getDSTDates(year) {
     const dstDates = [];
+	// DST Start
+	var firstOfMarch = new Date(year, 2, 1);
+	var daysUntilFirstSundayInMarch = (7 - firstOfMarch.getDay()) % 7;
+	var secondSundayInMarch = firstOfMarch.getDate() + daysUntilFirstSundayInMarch + 7;
+	var dstStartDate = new Date(year, 2, secondSundayInMarch);
+	// DST End
+	var firstOfNovember = new Date(year, 10, 1);
+	var daysUntilFirstSundayInNov = (7 - firstOfNovember.getDay()) % 7;
+	var firstSundayInNovember = firstOfNovember.getDate() + daysUntilFirstSundayInNov;
+	var dstEndDate = new Date(year, 10, firstSundayInNovember);
 
-    for (let year = startYear; year <= endYear; year++) {
-        // DST Start
-        var firstOfMarch = new Date(year, 2, 1);
-        var daysUntilFirstSundayInMarch = (7 - firstOfMarch.getDay()) % 7;
-        var secondSundayInMarch = firstOfMarch.getDate() + daysUntilFirstSundayInMarch + 7;
-        var dstStartDate = new Date(year, 2, secondSundayInMarch);
-
-        // DST End
-        var firstOfNovember = new Date(year, 10, 1);
-        var daysUntilFirstSundayInNov = (7 - firstOfNovember.getDay()) % 7;
-        var firstSundayInNovember = firstOfNovember.getDate() + daysUntilFirstSundayInNov;
-        var dstEndDate = new Date(year, 10, firstSundayInNovember);
-
-        dstDates.push({
-            year: year,
-            start: dstStartDate,
-            end: dstEndDate
-        });
-    }
+	dstDates.push({
+		year: year,
+		start: dstStartDate,
+		end: dstEndDate
+	});
 
     return dstDates;
 }
 
 function isEDT() {
+	const now = new Date();
+    const dstDates = getDSTDates(now.getFullYear());
 
-    const startYear = 2023;
-    const endYear = 2027;
-
-    const dstDates = getDSTDates(startYear, endYear);
-
-    dstDates.forEach(entry => {
-        console.log(`${entry.year}: DST starts on ${entry.start.toDateString()} and ends on ${entry.end.toDateString()}`);
+	var sstart = new Date();
+	var eend = new Date();
+	dstDates.forEach(entry => {
+        // console.log(`${entry.year}: DST starts on ${entry.start.toDateString()} and ends on ${entry.end.toDateString()}`);
+		sstart = entry.start
+		eend = entry.end
     });
-
-
-    const now = new Date();
-
-    // Get the current year's DST start and end dates
-    const dstStart = new Date(now.getFullYear()+1, 2, 8 - new Date(now.getFullYear(), 2, 1).getDay() + 7);
-    const dstEnd = new Date(now.getFullYear()+1, 10, 1 - new Date(now.getFullYear(), 10, 1).getDay() + 7);
-
-    console.log(dstStart)
-    console.log(dstEnd)
-
-    // Check if current date is between DST start and end dates
-    return now >= dstStart && now < dstEnd;
+    return now >= sstart && now < eend;
 }
 
-function shouldExecute() {
-
+function shouldExecute(localStartDate,localEndDate) {
     // Call the function to determine if it's EDT or EST
     if (isEDT()) {
-        console.log("It's Eastern Daylight Time (EDT).");
+        // console.log("It's Eastern Daylight Time (EDT).");
         var DaylightSavCorrection = -4
     } else {
-        console.log("It's Eastern Standard Time (EST).");
+        // console.log("It's Eastern Standard Time (EST).");
         var DaylightSavCorrection = -5
     }
-
-
     const currentDate = new Date();
-    console.log(currentDate);
-    
     // Convert current date and time to UTC to preserve the same point in time for all time zones
     const currentUTC = new Date(currentDate.getUTCFullYear(), currentDate.getUTCMonth(), currentDate.getUTCDate(), currentDate.getUTCHours(), currentDate.getUTCMinutes(), currentDate.getUTCSeconds());
-    console.log(currentUTC);
+    // console.log(currentUTC);
 
-    // Define the start and end dates and times in your local time zone
-    const localStartDate = new Date('2023-08-23T14:40:00'); // Replace with your desired start date and time in your local time zone
-    const localEndDate = new Date('2023-08-23T14:41:00');   // Replace with your desired end date and time in your local time zone
-    
-    // Convert local times to Eastern Standard Time (EST) since you're interested in that time zone
+	// Convert local times to Eastern Standard Time (EST) since you're interested in that time zone
     const estOffset = DaylightSavCorrection; // EDT offset is -4 hours from UTC; EST offset is -5 hours from UTC
     const estStartDate = new Date(localStartDate.getTime() - estOffset * 60 * 60 * 1000);
     const estEndDate = new Date(localEndDate.getTime() - estOffset * 60 * 60 * 1000);
     
     return currentUTC >= estStartDate && currentUTC <= estEndDate;
 }
-
-function ShowBanner() {
-    if (shouldExecute()) {
-        // Your code here that should be executed between the specified date and time range
-        console.log("ShowBanner executed!");
-    } else {
-        // Code to execute when the current date/time is outside the specified range
-        console.log("ShowBanner skipped!");
-    }
-}
-
-
 
 
 $(window).load(function () {
@@ -148,18 +112,7 @@ $(window).load(function () {
 		$( ".dropdown-content" ).insertAfter( '#dropicon' );
 		$( "a[href='basket_view.cgi'].no_mobile" ).insertBefore( "#dropdowndiv" );
 		$( ".image-container img").css("max-height","max-content");
-		
 		$( "#desklogo" ).insertBefore(".header-links");
-		/*$( "#desklogo" ).after('<td id="allitemsTD" class="toptabs">' + $('#tab_responsive_tabs dfn:contains("All Items")').html());
-		$( "#allitemsTD" ).after('<td id="mensTD" class="toptabs">' + $('#tab_responsive_tabs dfn:contains("Mens")').html());
-		$( "#mensTD" ).after('<td id="womensTD" class="toptabs">' + $('#tab_responsive_tabs dfn:contains("Womens")').html());
-		$( "#womensTD" ).after('<td id="unisexTD" class="toptabs">' + $('#tab_responsive_tabs dfn:contains("Unisex")').html());
-		$( "#unisexTD" ).after('<td id="accessoriesTD" class="toptabs">' + $('#tab_responsive_tabs dfn:contains("Accessories")').html());
-		$( "#accessoriesTD" ).after('<td id="marketingTD" class="toptabs">' + $('#tab_responsive_tabs dfn:contains("Marketing")').html());
-		$( "#marketingTD" ).after('<td id="faqTD" class="toptabs">' + $('#tab_responsive_tabs dfn:contains("FAQ")').html());
-		$( "#faqTD" ).after('<td id="space">');
-		*/
-
 
 		var idN = 'desklogo';
 		var idN2 = '';
@@ -171,55 +124,7 @@ $(window).load(function () {
 		});
 		$('#' + idN).after('<td id="space">');
 
-
-
 		$( ".toptabs:contains('undefined')" ).hide();
-		
-		/*
-		var nostar = $('td.numcol.pricecol:contains("*")').text().replace("*", "").replace(" ", "");
-		$('td.numcol.pricecol:contains("*")').text(nostar);
-		$('td.left.middle.qty_select_cell:contains("-$0.00")').each(function() {
-			$(this).text("1");
-			$(this).css("padding-left", "8px");
-			$(this).parents('tr').appendTo($(this).parents("tbody"));
-		});
-		var freeship = $('tr.form-sublabel:contains("Free Shipping over $100.00")');
-		if($(freeship).length > 0) {
-			var tbl = $('tr.form-sublabel').last();
-			$(freeship).insertAfter(tbl);
-			$('td.center:contains("Free shipping provided due to discount")').hide();
-		}*/
-		
-		
-		
-		/*
-		var total_cart = 0;
-		$('td.left.no_mobile:contains("$")').each(function() {
-			var numb = $( this ).text().replace("$", "").replace(" ea.", "");
-			if(isNaN(numb)) {
-				numb = 0;
-			}
-			total_cart = total_cart + parseFloat(numb);
-		});
-		total_cart = total_cart.toFixed(2);
-		$('<tr><td></td><td class="left middle qty_select_cell" style="padding-right: 20px; text-align: right !important; font-weight: bold;">Total</td><td class="left no_mobile"><span class="note" style="font-weight: bold;">$'+total_cart+'</span><td></td>').appendTo($('table#shipping-pricetable').children('tbody'));
-		console.log($('#review-items-table').length);
-		if($('#review-items-table').length === 0) {
-			total_cart = 0;
-			if($('table.items-table.true-table').length > 0) {
-				$('td:contains("$")').each(function() {
-					var numb = $( this ).text().replace("$", "");
-					if(isNaN(numb)) {
-						numb = 0;
-					}
-					total_cart = total_cart + parseFloat(numb);
-				});
-				total_cart = total_cart.toFixed(2);
-				$('<tr><td></td><td class="right" style="font-weight: bold;">Total</td><td class="right"><span class="note" style="font-weight: bold;">$'+total_cart+'</span><td></td>').appendTo($('table.items-table.true-table').children('tbody'));
-			}
-		}*/
-		
-		
 		
 		if($('.pagetitle').text().toUpperCase() === 'IN YOUR CART') {
 			var cnt = 1;
@@ -261,23 +166,20 @@ $(document).ready(function () {
 		$("button#submit_cancel").hide();
 	}
     
-    
-    // const edate = new Date();
-    // var ESToffSet = -240; //EST is -4:00 of UTC; i.e. 60*4 = -240 in minutes 
-    // offset= ESToffSet*60*1000;
-    // var ESTTime = new Date(edate.getTime()+offset);
-    // console.log("EST Date",ESTTime);
-
-    // const date = new Date();
-    // var CSToffSet = -360; //CST is -6:00 of UTC; i.e. 60*6 = -360 in minutes 
-    // offset= CSToffSet*60*1000;
-    // var CSTTime = new Date(date.getTime()+offset);
-    // console.log("CST Date",CSTTime);
-
-    // console.log("Date Now: ",Date(Date.now()));
-    // console.log(Date(Date.now()));
-
-    ShowBanner();
+	// Show Banner only during a predetermined date and time.
+    var localStartDate = new Date('2023-08-31T00:00:00'); // Replace with your desired start date and time in your local time zone
+    var localEndDate = new Date('2023-08-31T23:59:59');   // Replace with your desired end date and time in your local time zone
+	if (shouldExecute(localStartDate,localEndDate)) {
+		console.log("Start - " + localStartDate.toString());
+		console.log("End - " + localEndDate.toString());
+		var dnow = new Date();
+		console.log("Now - " + dnow.toString());
+		console.log("Show Banner");
+		$("#salebanner").show();
+	} else {
+		console.log("Hide Banner");
+		$("#salebanner").hide();
+	}
 
     if ($("#salebanner").length > 0) {
         var classstr = $("#salebanner").parents("div").attr("class");
@@ -285,6 +187,7 @@ $(document).ready(function () {
             $("#salebanner").parents("div").removeClass("no_mobile");
         }    
     }
+	//End Banner ************************************************************
 });
 function windowSize() {
 	windowHeight = window.innerHeight ? window.innerHeight : $(window).height();
