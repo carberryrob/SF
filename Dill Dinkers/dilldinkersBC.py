27e63142211dcdcb88ef1f45132ab5759abc3a04
carberryrob <rcarberry@strategicfactory.com>
2023-08-28 12:49:08 -0400

    Changes to be committed: 	modified:   Dill Dinkers/dilldinkers.css 	modified:   Dill Dinkers/dilldinkersBC.py

--- Dill Dinkers/dilldinkersBC.py
+++ Dill Dinkers/dilldinkersBC.py
@@ -24,4 +24,4 @@ MainText = DWIMLINES([LEFT, F2, name_first, IF(' ', name_middle), IF(' ', name_l
         VSPACER(vspc), F8, 'www.dilldinkers.com', NEWLINE],[], (1.95*72,1.75*72))
 
 # MainBlock = DWIMBLOCK([MainText], (1.4*72, 1.631945*72), (LEFT, TOP_BASELINE), (1.85*72,0), layer=0)
-MainBlock = DWIMBLOCK([MainText], (1.4*72, 1*72), (LEFT, MIDDLE), (1.95*72,1.75*72), layer=0)
+MainBlock = DWIMBLOCK([MainText], (1.45*72, 1*72), (LEFT, MIDDLE), (1.95*72,1.75*72), layer=0)
