fd9d2991f8212a7fff6aca1ee6e85347af559311
carberryrob <rcarberry@strategicfactory.com>
2023-09-05 13:55:34 -0400

    Changes to be committed: 	new file:   AAA MidAtlantic/AAA.JS 	new file:   AAA MidAtlantic/AAA.css 	modified:   Handling.py

--- Handling.py
+++ Handling.py
@@ -2,3 +2,11 @@
 [i for i in order.items if i.is_inventory_item()]
 
 [i for i in order.items if i.is_inventory_item() and order.form.corp_tag in ('merch', 'merch-comm')]
+
+
+
+Any Item:
+item.corp_filter == 'AAA' and item.tag in ('AAA-PRM-TENT-001', 'AAA-PRM-TBLCVR-001', 'AAA-PRM-BAN-001') $43
+item.corp_filter == 'AAA' and item.tag not in ('AAA-PRM-TENT-001', 'AAA-PRM-TBLCVR-001', 'AAA-PRM-BAN-001') $8
+
+[i for i in order.items if i.corp_filter not in ('AAA') and i.is_inventory_item()]
