c2756c698f3d23e43f6b4ded68d5b4940db01aa2
carberryrob <rcarberry@strategicfactory.com>
2023-09-29 13:20:22 -0400

    Changes to be committed: 	modified:   Catholic Charities/BC.py

--- Catholic Charities/BC.py
+++ Catholic Charities/BC.py
@@ -1,17 +1,18 @@
-LEFT, F1, name_first, IF(' ', name_last), IF(WRAPHERE(' '), credentials), NEWLINE,
-F2, WRAP, card_title1, NOWRAP, NEWLINE,
-VSPACER(11.85), F3, card_street1, NEWLINE,
-card_street2, NEWLINE,
-card_city, IF(', ', card_state), IF(' ', card_zip), NEWLINE,
-F4, IFSET(Phone1Word, JOIN3('T  ', COL, Phone1Word)), NEWLINE,
-IFSET(Phone2Word, JOIN3('M  ', COL, Phone2Word)), NEWLINE,
-IFSET(Phone3Word, JOIN3('F  ', COL, Phone3Word)), NEWLINE,
-F3, card_email, NEWLINE,
-card_www, NEWLINE
 
+# LEFT, F1, name_first, IF(' ', name_last), IF(WRAPHERE(' '), credentials), NEWLINE,
+# F2, WRAP, card_title1, NOWRAP, NEWLINE,
+# VSPACER(11.85), F3, card_street1, NEWLINE,
+# card_street2, NEWLINE,
+# card_city, IF(', ', card_state), IF(' ', card_zip), NEWLINE,
+# F4, IFSET(Phone1Word, JOIN3('T  ', COL, Phone1Word)), NEWLINE,
+# IFSET(Phone2Word, JOIN3('M  ', COL, Phone2Word)), NEWLINE,
+# IFSET(Phone3Word, JOIN3('F  ', COL, Phone3Word)), NEWLINE,
+# F3, card_email, NEWLINE,
+# card_www, NEWLINE
 
 
 # Enable logic before blocks section
+VS = 11.85
 
 AddressBlock = DWIMBLOCK([LEFT, F3, card_street1, NEWLINE,
 card_street2, NEWLINE,
@@ -20,11 +21,13 @@ F4, IFSET(Phone1Word, JOIN3('T  ', COL, Phone1Word)), NEWLINE,
 IFSET(Phone2Word, JOIN3('M  ', COL, Phone2Word)), NEWLINE,
 IFSET(Phone3Word, JOIN3('F  ', COL, Phone3Word)), NEWLINE,
 F3, WRAPON(card_email, '@', wrap_before =1), NEWLINE,
-card_www, NEWLINE], (1.85*72, 1*72), (LEFT, MIDDLE), (1.7*72,1.75*72), layer=0)
+card_www, NEWLINE], (1.85*72, 1*72), (LEFT, MIDDLE), (1.7*72,1.95*72), layer=0)
 
 
 # Reduce font until address blocks fits.
 while (AddressBlock.width > (1.5*72)):
+    VS -= 0.01
+    F1.pointsize = F1.pointsize - 0.005
     F3.pointsize = F3.pointsize - 0.025
     F4.size1 = F4.size1 - 0.025
     F4.size2 = F4.size2 - 0.025
@@ -36,12 +39,41 @@ while (AddressBlock.width > (1.5*72)):
     IFSET(Phone2Word, JOIN3('M  ', COL, Phone2Word)), NEWLINE,
     IFSET(Phone3Word, JOIN3('F  ', COL, Phone3Word)), NEWLINE,
     F3, WRAPON(card_email, '@', wrap_before =1), NEWLINE,
-    card_www, NEWLINE], (1.85*72, 1*72), (LEFT, MIDDLE), (1.7*72,1.75*72), layer=0)
+    card_www, NEWLINE], (1.85*72, 1*72), (LEFT, MIDDLE), (1.7*72,1.95*72), layer=0)
 
 # Write the complete text box
 AddressBlock = DWIMBLOCK([LEFT, F1, name_first, IF(' ', name_last), IF(WRAPHERE(' '), credentials), NEWLINE,
 F2, WRAP, card_title1, NOWRAP, NEWLINE,
-VSPACER(11.85), F3, card_street1, NEWLINE,
+VSPACER(VS), F3, card_street1, NEWLINE,
+card_street2, NEWLINE,
+card_city, IF(', ', card_state), IF(' ', card_zip), NEWLINE,
+F4, IFSET(Phone1Word, JOIN3('T  ', COL, Phone1Word)), NEWLINE,
+IFSET(Phone2Word, JOIN3('M  ', COL, Phone2Word)), NEWLINE,
+IFSET(Phone3Word, JOIN3('F  ', COL, Phone3Word)), NEWLINE,
+F3, WRAPON(card_email, '@', wrap_before =1), NEWLINE,
+card_www, NEWLINE], (1.85*72, 1*72), (LEFT, MIDDLE), (1.5*72,1.95*72), layer=0)
+
+while (AddressBlock.height > (1.75*72)):
+    VS -= 0.05
+    F1.pointsize = F1.pointsize - 0.005
+    F3.pointsize = F3.pointsize - 0.015
+    F4.size1 = F4.size1 - 0.015
+    F4.size2 = F4.size2 - 0.015
+
+    AddressBlock = DWIMBLOCK([LEFT, F1, name_first, IF(' ', name_last), IF(WRAPHERE(' '), credentials), NEWLINE,
+    F2, WRAP, card_title1, NOWRAP, NEWLINE,
+    VSPACER(VS), F3, card_street1, NEWLINE,
+    card_street2, NEWLINE,
+    card_city, IF(', ', card_state), IF(' ', card_zip), NEWLINE,
+    F4, IFSET(Phone1Word, JOIN3('T  ', COL, Phone1Word)), NEWLINE,
+    IFSET(Phone2Word, JOIN3('M  ', COL, Phone2Word)), NEWLINE,
+    IFSET(Phone3Word, JOIN3('F  ', COL, Phone3Word)), NEWLINE,
+    F3, WRAPON(card_email, '@', wrap_before =1), NEWLINE,
+    card_www, NEWLINE], (1.85*72, 1*72), (LEFT, MIDDLE), (1.5*72,1.95*72), layer=0)
+
+AddressBlock = DWIMBLOCK([LEFT, F1, name_first, IF(' ', name_last), IF(WRAPHERE(' '), credentials), NEWLINE,
+F2, WRAP, card_title1, NOWRAP, NEWLINE,
+VSPACER(VS), F3, card_street1, NEWLINE,
 card_street2, NEWLINE,
 card_city, IF(', ', card_state), IF(' ', card_zip), NEWLINE,
 F4, IFSET(Phone1Word, JOIN3('T  ', COL, Phone1Word)), NEWLINE,
@@ -50,4 +82,4 @@ IFSET(Phone3Word, JOIN3('F  ', COL, Phone3Word)), NEWLINE,
 F3, WRAPON(card_email, '@', wrap_before =1), NEWLINE,
 card_www, NEWLINE], (1.85*72, 1*72), (LEFT, MIDDLE), (1.5*72,1.75*72), layer=0)
 
-yBlock = DWIMBLOCK([RIGHT, F3, str(F3.pointsize) + " | " + str(F4.size1) + " | " + str(F4.size2), NEWLINE], (3.5*72, 0), (RIGHT, BOTTOM), layer=1)
+# yBlock = DWIMBLOCK([RIGHT, F3, str(F1.pointsize) + " | " + str(F3.pointsize) + " | " + str(F4.size1) + " | " + str(F4.size2) + " | " + str(VS), NEWLINE], (3.5*72, 0), (RIGHT, BOTTOM), layer=1)
