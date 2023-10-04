f9846444fa3dc9245c828dabb20bf5a46144c105
carberryrob <rcarberry@strategicfactory.com>
2023-10-04 01:29:58 -0400

    Changes to be committed: 	modified:   Catholic Charities/BC.py 	modified:   Catholic Charities/Catholic-Charities.css 	modified:   UCONNECT/UConnect CSS 	modified:   UCONNECT/UConnect JS 	new file:   UCONNECT/UConnect JS copy

--- Catholic Charities/BC.py
+++ Catholic Charities/BC.py
@@ -1,17 +1,7 @@
-
-# LEFT, F1, name_first, IF(' ', name_last), IF(WRAPHERE(' '), credentials), NEWLINE,
-# F2, WRAP, card_title1, NOWRAP, NEWLINE,
-# VSPACER(11.85), F3, card_street1, NEWLINE,
-# card_street2, NEWLINE,
-# card_city, IF(', ', card_state), IF(' ', card_zip), NEWLINE,
-# F4, IFSET(Phone1Word, JOIN3('T  ', COL, Phone1Word)), NEWLINE,
-# IFSET(Phone2Word, JOIN3('M  ', COL, Phone2Word)), NEWLINE,
-# IFSET(Phone3Word, JOIN3('F  ', COL, Phone3Word)), NEWLINE,
-# F3, card_email, NEWLINE,
-# card_www, NEWLINE
-
-
 # Enable logic before blocks section
+
+card_www = 'cc-md.org'
+
 VS = 11.85
 
 AddressBlock = DWIMBLOCK([LEFT, F3, card_street1, NEWLINE,
@@ -42,7 +32,7 @@ while (AddressBlock.width > (1.5*72)):
     card_www, NEWLINE], (1.85*72, 1*72), (LEFT, MIDDLE), (1.7*72,1.95*72), layer=0)
 
 # Write the complete text box
-AddressBlock = DWIMBLOCK([LEFT, F1, name_first, IF(' ', name_last), IF(WRAPHERE(' '), credentials), NEWLINE,
+AddressBlock = DWIMBLOCK([LEFT, F1, name_first, IF(' ', name_last), F5, IF(WRAPHERE(', '), credentials), NEWLINE,
 F2, WRAP, card_title1, NOWRAP, NEWLINE,
 VSPACER(VS), F3, card_street1, NEWLINE,
 card_street2, NEWLINE,
@@ -60,7 +50,7 @@ while (AddressBlock.height > (1.75*72)):
     F4.size1 = F4.size1 - 0.015
     F4.size2 = F4.size2 - 0.015
 
-    AddressBlock = DWIMBLOCK([LEFT, F1, name_first, IF(' ', name_last), IF(WRAPHERE(' '), credentials), NEWLINE,
+    AddressBlock = DWIMBLOCK([LEFT, F1, name_first, IF(' ', name_last), F5, IF(WRAPHERE(', '), credentials), NEWLINE,
     F2, WRAP, card_title1, NOWRAP, NEWLINE,
     VSPACER(VS), F3, card_street1, NEWLINE,
     card_street2, NEWLINE,
@@ -71,8 +61,9 @@ while (AddressBlock.height > (1.75*72)):
     F3, WRAPON(card_email, '@', wrap_before =1), NEWLINE,
     card_www, NEWLINE], (1.85*72, 1*72), (LEFT, MIDDLE), (1.5*72,1.95*72), layer=0)
 
-AddressBlock = DWIMBLOCK([LEFT, F1, name_first, IF(' ', name_last), IF(WRAPHERE(' '), credentials), NEWLINE,
-F2, WRAP, card_title1, NOWRAP, NEWLINE,
+NameBlock = DWIMBLOCK([LEFT, F1, name_first, IF(' ', name_last), F5, IF(WRAPHERE(', '), credentials), NEWLINE], (1.85*72, 2*72), (LEFT, TOP), (1.5*72,1.75*72), uniform_linescale=1, layer=0)
+
+AddressBlock = DWIMBLOCK([LEFT, F2, WRAP, card_title1, NOWRAP, NEWLINE,
 VSPACER(VS), F3, card_street1, NEWLINE,
 card_street2, NEWLINE,
 card_city, IF(', ', card_state), IF(' ', card_zip), NEWLINE,
@@ -80,6 +71,12 @@ F4, IFSET(Phone1Word, JOIN3('T  ', COL, Phone1Word)), NEWLINE,
 IFSET(Phone2Word, JOIN3('M  ', COL, Phone2Word)), NEWLINE,
 IFSET(Phone3Word, JOIN3('F  ', COL, Phone3Word)), NEWLINE,
 F3, WRAPON(card_email, '@', wrap_before =1), NEWLINE,
-card_www, NEWLINE], (1.85*72, 1*72), (LEFT, MIDDLE), (1.5*72,1.75*72), layer=0)
+card_www, NEWLINE], (1.85*72, NameBlock._get_bottom()-4.0), (LEFT, TOP), (1.5*72,1.75*72), layer=0)
 
-# yBlock = DWIMBLOCK([RIGHT, F3, str(F1.pointsize) + " | " + str(F3.pointsize) + " | " + str(F4.size1) + " | " + str(F4.size2) + " | " + str(VS), NEWLINE], (3.5*72, 0), (RIGHT, BOTTOM), layer=1)
+mb = -((146 - (NameBlock.height + AddressBlock.height)) / 2)
+AddressBlock.move(0,mb)
+NameBlock.move(0,mb)
+
+# aBlock = DWIMBLOCK([RIGHT, F3, str(NameBlock._get_top()) + " | " + str(NameBlock._get_bottom()) + " | " + str(NameBlock._get_bottom()-VS) + " | " + str(AddressBlock._get_top()) + " | " + str(VS), NEWLINE], (3.5*72, 0), (RIGHT, BOTTOM), layer=0)
+
+# bBlock = DWIMBLOCK([RIGHT, F3, str(mb) + " | " + str(F1.pointsize) + " | " + str(F3.pointsize) + " | " + str(F4.size1) + " | " + str(F4.size2) + " | " + str(VS), NEWLINE], (3.5*72, 9), (RIGHT, BOTTOM), layer=0)
