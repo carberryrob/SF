83e6cc2b4af8083f1aaee5b55e12a10ecca9be81
carberryrob <rcarberry@strategicfactory.com>
2023-10-08 20:01:52 -0400

    Changes to be committed: 	modified:   CFG/CFG.css 	modified:   Catholic Charities/BC.py 	modified:   Catholic Charities/Catholic-Charities.css 	modified:   UCONNECT/UConnect CSS 	modified:   UCONNECT/UConnect JS 	new file:   WYG/WYG.css 	new file:   WYG/WYG.js

--- Catholic Charities/BC.py
+++ Catholic Charities/BC.py
@@ -10,7 +10,7 @@ card_city, IF(', ', card_state), IF(' ', card_zip), NEWLINE,
 F4, IFSET(Phone1Word, JOIN3('T  ', COL, Phone1Word)), NEWLINE,
 IFSET(Phone2Word, JOIN3('M  ', COL, Phone2Word)), NEWLINE,
 IFSET(Phone3Word, JOIN3('F  ', COL, Phone3Word)), NEWLINE,
-F3, WRAPON(card_email, '@', wrap_before =1), NEWLINE,
+F3, CASE('lower'), WRAPON(card_email, '@', wrap_before =1), CASE('None'), NEWLINE,
 card_www, NEWLINE], (1.85*72, 1*72), (LEFT, MIDDLE), (1.7*72,1.95*72), layer=0)
 
 
@@ -28,19 +28,19 @@ while (AddressBlock.width > (1.5*72)):
     F4, IFSET(Phone1Word, JOIN3('T  ', COL, Phone1Word)), NEWLINE,
     IFSET(Phone2Word, JOIN3('M  ', COL, Phone2Word)), NEWLINE,
     IFSET(Phone3Word, JOIN3('F  ', COL, Phone3Word)), NEWLINE,
-    F3, WRAPON(card_email, '@', wrap_before =1), NEWLINE,
+    F3, CASE('lower'), WRAPON(card_email, '@', wrap_before =1), CASE('None'), NEWLINE,
     card_www, NEWLINE], (1.85*72, 1*72), (LEFT, MIDDLE), (1.7*72,1.95*72), layer=0)
 
 # Write the complete text box
 AddressBlock = DWIMBLOCK([LEFT, F1, name_first, IF(' ', name_last), F5, IF(WRAPHERE(', '), credentials), NEWLINE,
-F2, WRAP, card_title1, NOWRAP, NEWLINE,
+F2, WRAP, CASE('upper'), card_title1, CASE('None'), NOWRAP, NEWLINE,
 VSPACER(VS), F3, card_street1, NEWLINE,
 card_street2, NEWLINE,
 card_city, IF(', ', card_state), IF(' ', card_zip), NEWLINE,
 F4, IFSET(Phone1Word, JOIN3('T  ', COL, Phone1Word)), NEWLINE,
 IFSET(Phone2Word, JOIN3('M  ', COL, Phone2Word)), NEWLINE,
 IFSET(Phone3Word, JOIN3('F  ', COL, Phone3Word)), NEWLINE,
-F3, WRAPON(card_email, '@', wrap_before =1), NEWLINE,
+F3, CASE('lower'), WRAPON(card_email, '@', wrap_before =1), CASE('None'), NEWLINE,
 card_www, NEWLINE], (1.85*72, 1*72), (LEFT, MIDDLE), (1.5*72,1.95*72), layer=0)
 
 while (AddressBlock.height > (1.75*72)):
@@ -51,26 +51,26 @@ while (AddressBlock.height > (1.75*72)):
     F4.size2 = F4.size2 - 0.015
 
     AddressBlock = DWIMBLOCK([LEFT, F1, name_first, IF(' ', name_last), F5, IF(WRAPHERE(', '), credentials), NEWLINE,
-    F2, WRAP, card_title1, NOWRAP, NEWLINE,
+    F2, WRAP, CASE('upper'), card_title1, CASE('None'), NOWRAP, NEWLINE,
     VSPACER(VS), F3, card_street1, NEWLINE,
     card_street2, NEWLINE,
     card_city, IF(', ', card_state), IF(' ', card_zip), NEWLINE,
     F4, IFSET(Phone1Word, JOIN3('T  ', COL, Phone1Word)), NEWLINE,
     IFSET(Phone2Word, JOIN3('M  ', COL, Phone2Word)), NEWLINE,
     IFSET(Phone3Word, JOIN3('F  ', COL, Phone3Word)), NEWLINE,
-    F3, WRAPON(card_email, '@', wrap_before =1), NEWLINE,
+    F3, CASE('lower'), WRAPON(card_email, '@', wrap_before =1), CASE('None'), NEWLINE,
     card_www, NEWLINE], (1.85*72, 1*72), (LEFT, MIDDLE), (1.5*72,1.95*72), layer=0)
 
 NameBlock = DWIMBLOCK([LEFT, F1, name_first, IF(' ', name_last), F5, IF(WRAPHERE(', '), credentials), NEWLINE], (1.85*72, 2*72), (LEFT, TOP), (1.5*72,1.75*72), uniform_linescale=1, layer=0)
 
-AddressBlock = DWIMBLOCK([LEFT, F2, WRAP, card_title1, NOWRAP, NEWLINE,
+AddressBlock = DWIMBLOCK([LEFT, F2, WRAP, CASE('upper'), card_title1, CASE('None'), NOWRAP, NEWLINE,
 VSPACER(VS), F3, card_street1, NEWLINE,
 card_street2, NEWLINE,
 card_city, IF(', ', card_state), IF(' ', card_zip), NEWLINE,
 F4, IFSET(Phone1Word, JOIN3('T  ', COL, Phone1Word)), NEWLINE,
 IFSET(Phone2Word, JOIN3('M  ', COL, Phone2Word)), NEWLINE,
 IFSET(Phone3Word, JOIN3('F  ', COL, Phone3Word)), NEWLINE,
-F3, WRAPON(card_email, '@', wrap_before =1), NEWLINE,
+F3, CASE('lower'), WRAPON(card_email, '@', wrap_before =1), CASE('None'), NEWLINE,
 card_www, NEWLINE], (1.85*72, NameBlock._get_bottom()-4.0), (LEFT, TOP), (1.5*72,1.75*72), layer=0)
 
 mb = -((146 - (NameBlock.height + AddressBlock.height)) / 2)
---
+++ WYG/WYG.js
@@ -0,0 +1,117 @@
+<div class="footer_row3">
+<div><img style="opacity: 100%;" src="con/White-PoweredBySF.png" alt="Online Storefront Powered by Strategic Factory" class="img-fluid mt-3"></div>
+    <p class="footer_cr">
+        Strategic Factory<br>
+        11195 Dolfield Boulevard<br>
+        Owings Mills, Maryland 21117<br>
+        (443) 548-3500<br>
+        <a href="mailto:sfportalorders@strategicfactory.com">sfportalorders@strategicfactory.com</a>
+    </p>
+</div>
+
+<script>
+$(window).load(function () {
+	$("table#billing-info").insertBefore("div#final-buttonsbar"); /* *** Enable/Disable "Submit Order" button based on approval move to above Submit*** */
+    $('head').append('<link rel="icon" type="image/png" sizes="32x32" href="https://whatsyourgrief.com/favicon-32x32.png">');
+	$( "link[href^='css/header.css']" ).removeAttr( "media" );
+	$( "#header_logo img.no_mobile" ).wrap( '<a style="display: inline-block;" href="index.cgi">' );
+	$( "#header_logo img.no_desktop" ).wrap( '<a style="display: inline-block;" href="index.cgi">' );
+	$(".note:contains('Non-taxable items marked with *')").hide();
+	//var nostar = $('td.numcol.pricecol.discount_price:contains("*")').text().replace("*", "");
+	//$('td.numcol.pricecol.discount_price:contains("*")').text(nostar);
+	//var nostar = $('td.numcol.pricecol:contains("*")').text().replace("*", "");
+	//$('td.numcol.pricecol:contains("*")').text(nostar);
+	//$('tr.form-sublabel.negativeprice').insertAfter('#bill.shipping_label').parent('tr');
+		
+	/*if ($("tr.form-sublabel.negativeprice:contains('Voucher Funds')").length > 0) {
+		$("tr.form-sublabel.negativeprice td:contains('Voucher Funds')").css('font-weight', 'bold');
+		var voucher = $("tr.form-sublabel.negativeprice:contains('Voucher Funds')");
+		var ttl = $(voucher).siblings('tr:contains("Total Price:")');
+		if ($(ttl).length > 0) {$(voucher).insertBefore($(ttl));}
+		var cc = $(voucher).siblings('tr:contains("Creditcard Surcharge:")');
+		if ($(cc).length > 0) {$(voucher).insertBefore($(cc));}
+		
+	}*/
+	//$("table#billing-pricetable").find("tr:contains('Handling')").hide();
+	$("table#billing-pricetable").find("td , th").each(function() {
+		
+		$(this).css("text-align", "right");
+		$(this).filter(":contains('*')").each(function() {
+			var nostar = '';
+			var nostar = $(this).html().replace("*", "");
+			$(this).html(nostar);
+		});
+	});
+    if($(window).width() >= 1025) {
+		$( "#desklogo" ).insertBefore(".header-links");
+		$('table.items-table.true-table').children().find("th").each(function() {
+			if($(this).text().length > 0 && $(this).attr("class") === undefined) {
+				//console.log($(this).attr("class") + " | " + $(this).text());
+				$(this).attr("class", "right");
+				//console.log($(this).attr("class") + " | " + $(this).text());
+			}			
+		});
+		$('table.items-table.true-table').children().find('td').each(function() {
+			if($(this).text().length > 0 && $(this).attr("class") === undefined) {
+				//console.log($(this).attr("class") + " | " + $(this).text());
+				$(this).attr("class", "right");
+				//console.log($(this).attr("class") + " | " + $(this).text());
+			}			
+		});
+
+		if($('.pagetitle').text().toUpperCase() === 'IN YOUR CART') {
+			var cnt = 1;
+			$('span.menu-knob').parents('tr').each(function() {
+				$( this ).attr('class', 'menu-knob_tr'+cnt);
+				$('<td class="cart_item"></td>').insertBefore( this);
+				$( this ).children('th').css('text-align', 'center')
+				cnt = cnt + 1;
+			});
+			cnt = 1;
+			$('.previewcell').parents('tr').each(function() {
+				$( this ).attr('class', 'previewcell_tr'+cnt);
+				cnt = cnt + 1;
+			});
+			cnt = 1;
+			$('.cart_item').each(function() {
+				var previewcell = $('.previewcell_tr'+cnt)
+				var menuknob = $('.menu-knob_tr'+cnt)
+				$( this).prepend(previewcell);
+				$( this).prepend(menuknob);
+				cnt = cnt + 1;
+			});
+			$('.cart_item').parents('table').css('text-align', 'center');
+		}
+	}
+});
+$(document).ready(function () {
+	//$( "#shipmeth-info" ).hide();
+	if($('form[action="profile.cgi"]').length === 0) {
+		if( $(".item_count").text() > 0) {
+			$("button#submit_cancel").show();
+		} else{
+			$("button#submit_cancel").hide();
+		}
+	}
+});
+</script>
+<script>
+//********** Enable/Disable "Submit Order" button based on approval *****************************************************
+$(document).ready(function () {
+	if ($("select[name='bill_code10']").val() == "No") {
+		$("button#submit_send_order").prop('disabled', 'disabled');
+	}
+	//console.log($("select[name='bill_code10']").val());
+	//$('form[action="accept_bill.cgi"]').change(function() {
+	$('form[action="accept.cgi"]').change(function() {
+		if ($("select[name='bill_code10']").val() == "No") {$("button#submit_send_order").prop('disabled', 'disabled');}
+		if ($("select[name='bill_code10']").val() == "Yes") {$("button#submit_send_order").prop('disabled', false);}
+		//console.log($("select[name='bill_code10']").val());
+	});
+
+	//********** Move approval yes/no to just above "Submit Order"*******************
+	$("tr:contains('I have verified and approve this order:')").appendTo($("p.approve_order").parents("tr").parent());
+	//*******************************************************************************
+});
+//***********************************************************************************************************************
+</script>
