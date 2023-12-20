ae6273af5c73f45d893695b3d5994bef821e1cc8
carberryrob <rcarberry@strategicfactory.com>
2023-12-20 11:21:47 -0500

    Changes to be committed: 	modified:   CFG/CFG.css 	new file:   Dill Dinkers/dilldinkers copy.css 	new file:   Stollarstore/bundle.js 	modified:   UCONNECT/UConnect JS

---
+++ Stollarstore/bundle.js
@@ -0,0 +1,22 @@
+
+$(window).load(function () {
+
+	// if($(window).width() >= 1025) {
+
+
+		// Make Kit Qty read only
+    $("table.true-table .qty_select_cell > select[name*='_Bundle']").each(function() {
+        // console.log( $(this).val() );
+        $(this).closest('td').prepend($(this).val());
+        $(this).hide();
+    });
+    $("table.true-table .qty_select_cell > input[name*='_Bundle']").each(function() {
+        // console.log( $(this).val() );
+        $(this).closest('td').prepend($(this).val());
+        $(this).hide();
+    });
+	// }
+	
+	// $(".true-table").show();
+
+});
