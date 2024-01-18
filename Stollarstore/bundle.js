4da6c643f95781bb10e5743005c1d3062e3a7af9
carberryrob <rcarberry@strategicfactory.com>
2024-01-18 17:59:03 -0500

    Changes to be committed: 	modified:   Stollarstore/bundle.js

--- Stollarstore/bundle.js
+++ Stollarstore/bundle.js
@@ -1,10 +1,6 @@
-
-$(window).load(function () {
-
-	// if($(window).width() >= 1025) {
-
-
-		// Make Kit Qty read only
+$(document).ready(function () {
+// $(window).load(function () {
+		// Make Kit Qty read only in basket etc.
     $("table.true-table .qty_select_cell > select[name*='_Bundle']").each(function() {
         // console.log( $(this).val() );
         $(this).closest('td').prepend($(this).val());
@@ -15,8 +11,4 @@ $(window).load(function () {
         $(this).closest('td').prepend($(this).val());
         $(this).hide();
     });
-	// }
-	
-	// $(".true-table").show();
-
 });
