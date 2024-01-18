402ddf84740c71d2ee88658ff4152bf0b51f9ff4
carberryrob <rcarberry@strategicfactory.com>
2024-01-18 18:17:47 -0500

    Changes to be committed: 	modified:   Stollarstore/bundle.js

--- Stollarstore/bundle.js
+++ Stollarstore/bundle.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
         $(this).closest('td').prepend($(this).val());
         $(this).hide();
     });
-    $(input#ship_needby).each(function() {
+    $('input#ship_needby').each(function() {
         console.log($(this).val());
     });
 });
