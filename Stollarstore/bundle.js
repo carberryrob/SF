6f0150e769bb3fa15c8cb9098493a78de54f9b4b
carberryrob <rcarberry@strategicfactory.com>
2024-01-25 11:56:01 -0500

    Changes to be committed: 	modified:   Longname FMT on custome tab.txt 	modified:   Stollarstore/bundle.js 	modified:   UCONNECT/UConnect JS

--- Stollarstore/bundle.js
+++ Stollarstore/bundle.js
@@ -1,19 +1,29 @@
 $(document).ready(function () {
 // $(window).load(function () {
 		// Make Kit Qty read only in basket etc.
-    $("table.true-table .qty_select_cell > select[name*='_Bundle']").each(function() {
-        // console.log( $(this).val() );
-        $(this).closest('td').prepend($(this).val());
-        $(this).hide();
-    });
-    $("table.true-table .qty_select_cell > input[type='text'][name*='_Bundle']").each(function() {
-        // console.log( $(this).val() );
-        $(this).closest('td').prepend($(this).val());
-        $(this).hide();
-    });
-    $('input#ship_needby').each(function() {
-        // console.log($(this).val());
-        $(this).attr('autocomplete','off')
-        $(this).val('')
-    });
+     $("table.true-table .qty_select_cell > select[name*='_Bundle']").each(function() {
+          // console.log( $(this).val() );
+          $(this).closest('td').prepend($(this).val());
+          $(this).hide();
+     });
+     $("table.true-table .qty_select_cell > input[type='text'][name*='_Bundle']").each(function() {
+          // console.log( $(this).val() );
+          $(this).closest('td').prepend($(this).val());
+          $(this).hide();
+     });
+     $("table.true-table .qty_select_cell > select[name*='For_Kit']").each(function() {
+          // console.log( $(this).val() );
+          $(this).closest('td').prepend($(this).val());
+          $(this).hide();
+     });
+     $("table.true-table .qty_select_cell > input[type='text'][name*='For_Kit']").each(function() {
+          // console.log( $(this).val() );
+          $(this).closest('td').prepend($(this).val());
+          $(this).hide();
+     });
+     $('input#ship_needby').each(function() {
+          // console.log($(this).val());
+          $(this).attr('autocomplete','off')
+          $(this).val('')
+     });
 });
