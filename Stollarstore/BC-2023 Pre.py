308cf779513e13fa9a170dff2b3557c7bff5db8c
carberryrob <rcarberry@strategicfactory.com>
2024-02-16 14:03:22 -0500

    Changes to be committed: 	modified:   Dill Dinkers/dilldinkers.css 	modified:   Dill Dinkers/dilldinkers.js 	modified:   Stollarstore/BC-2023 Pre.py 	modified:   Stollarstore/index.html 	new file:   Top_Menu_Template/Top_Menu_Template copy.js 	modified:   Top_Menu_Template/Top_Menu_Template.css 	modified:   Top_Menu_Template/Top_Menu_Template.js 	new file:   UCONNECT/UConnect Appt Card Back COUA.py 	new file:   UCONNECT/UConnect Appt Card Front COUA.py 	modified:   UCONNECT/UConnect Appt Card Front.py

--- Stollarstore/BC-2023 Pre.py
+++ Stollarstore/BC-2023 Pre.py
@@ -9,18 +9,10 @@ else:
     T3 = T2
     T2 = T1
 if card_email:
+    card_email = card_email.replace(card_emailtag,'') + card_emailtag
     emailBlock = DWIMBLOCK([EPSWORD('con/email.pdf', xscale=1, yscale=1), LEFT, F1, SPACER(7), F5, card_email, NEWLINE], (0.1875 * 72, T2), (LEFT, TOP_BASELINE), layer=0)
 else:
     T3 = T2
     T2 = T1
 if card_street1 or card_street2 or card_city or card_state or card_zip:
     addressBlock = DWIMBLOCK([EPSWORD('con/address.pdf', xscale=1, yscale=1), LEFT, F1, SPACER(7), F5, card_street1, IF(', ', card_street2), IF(', ', card_city), IF(', ', card_state), IF(' ', card_zip), NEWLINE], (0.1875 * 72, T3), (LEFT, TOP_BASELINE), layer=0)
-
-
-
-bc_sf_2023-For_Kit
-bc_sf_2023-SpotUV-For_Kit
-
-
-
-LEFT, F7, name_first, IF(' ', name_last), NEWLINE
