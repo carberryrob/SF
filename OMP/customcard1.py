d24c7a762f52c3e2f68c0d0b182bf2c8f2e15e8d
carberryrob <rcarberry@strategicfactory.com>
2024-03-11 22:49:32 -0400

    Changes to be committed: 	modified:   OMP/OMP.css 	modified:   OMP/customcard1.py

--- OMP/customcard1.py
+++ OMP/customcard1.py
@@ -48,7 +48,7 @@ if (image):
           elif (style1_style == '3'):
                boxwidth = float(3.25*72)
                boxheight = float(.75*72)
-          Logo = DWIMLINES([BORDER(EPSWORD(image, xscale=1.0, resize=(boxwidth-4.0, boxheight-4.0), yscale=1.0), border_width, Black)])
+          Logo = DWIMLINES([BORDER(EPSWORD(image, xscale=1.0, resize=(boxwidth-4.0, boxheight-14.0), yscale=1.0), border_width, Black)])
           # Logo = DWIMLINES([BORDER(EPSWORD(image, xscale=1.0, resize=(boxwidth, boxheight), yscale=1.0), border_width, Black)])
           LogoBlock = DWIMBLOCK([Logo], (0, 0), (LEFT,TOP), layer=0)
           h = float(LogoBlock.height)
@@ -98,7 +98,7 @@ try:
           if (full_name or card_title1):
                Block2 = DWIMBLOCK([fx2, full_name, NEWLINE, card_title1, NEWLINE], (0, 144), (LEFT, TOP), (width,0), layer=0)
                bloks.append(Block2)
-          if (card_street1 or card_street2 or ph1 or ph2 or card_email or card_www):
+          if (card_street1 or card_street2 or card_street3 or ph1 or ph2 or card_email or card_www):
                Block3 = DWIMBLOCK([fx3, card_street1, NEWLINE, card_street2, NEWLINE, card_street3, NEWLINE, CHAIN(ph1, bullet2, ph2), NEWLINE, VSPACER(vspc), card_email, NEWLINE, card_www, NEWLINE], (0, 144), (LEFT, TOP), (width,0), uniform_linescale=1, layer=0)
                bloks.append(Block3)
           maxstart = 0
@@ -119,7 +119,7 @@ try:
                     bh = Block2._get_top()-Block2._get_bottom() - 1
                     Block2 = DWIMBLOCK([fx2, full_name, NEWLINE, card_title1, NEWLINE], (0, 144), (LEFT, TOP), (width,bh), layer=0)
                     bloks.append(Block2)
-               if (card_street1 or card_street2 or ph1 or ph2 or card_email or card_www):
+               if (card_street1 or card_street2 or card_street3 or ph1 or ph2 or card_email or card_www):
                     bh = Block3._get_top()-Block3._get_bottom() - 1
                     Block3 = DWIMBLOCK([fx3, card_street1, NEWLINE, card_street2, NEWLINE, card_street3, NEWLINE, CHAIN(ph1, bullet2, ph2), NEWLINE, VSPACER(vspc), card_email, NEWLINE, card_www, NEWLINE], (0, 144), (LEFT, TOP), (width,bh), uniform_linescale=1, layer=0)
                     bloks.append(Block3)
@@ -224,7 +224,7 @@ try:
           block = None
           # COND(str(bottom) + ' - ' + str(-top) + ' - ' + str(min(bottom, -(top))))
           top = min(bottom, -(top))
-          if (card_street1 or card_street2 or ph1 or ph2 or card_email or card_www):
+          if (card_street1 or card_street2 or card_street3 or ph1 or ph2 or card_email or card_www):
                Block3 = DWIMBLOCK([fx3, CHAIN(card_street1, bullet2, card_street2, bullet2, card_street3), NEWLINE, CHAIN(ph1, bullet2, ph2), NEWLINE, card_email, NEWLINE, card_www, NEWLINE], (9, 11), (LEFT, BOTTOM), (3.25*72,top-7), uniform_linescale=1, layer=0)
                # bloks.append(Block3)
 
@@ -242,9 +242,204 @@ try:
 
 
      elif (float(style1_style) == 3.0):
+          bloks = []
+          bloks2 = []
+          vspc = 0
+          if (image):
+               left = float(9.0)
+               width = float(3.25*72)
+               width2 = float((width - 9) / 2)
+               top = float(LogoBlock._get_bottom() - 9.0)
+               left2 = float((left*2) + (width2))
+          else:
+               left = float(9.0)
+               width = float(3.25*72)
+               width2 = float((width - 9) / 2)
+               top = float(72)
+               left2 = float((left*2) + (width2))
+          # height = float(((top / 2) / 2) - 9)
+          height = float((top - 12 ) / 4)
+          height2 = float((top - 12 ) / 2)
+          # COND(str(top) + ' - ' + str(top/72))
           if (card_company or card_department):
-               Block1 = DWIMBLOCK([fx, card_company, NEWLINE, card_department, NEWLINE], (0, 144), (LEFT, TOP), (0,0), layer=0)
-               bloks.append(Block1)     
+               Block1 = DWIMBLOCK([fx, card_company, NEWLINE, card_department, NEWLINE], (left, top), (LEFT, TOP), (width,height), layer=0)
+               bloks.append(Block1)
+               top = float(Block1._get_bottom()-7)
+          if (full_name or card_title1):
+               Block2 = DWIMBLOCK([fx2, full_name, NEWLINE, card_title1, NEWLINE], (left, top), (LEFT, TOP), (width,height), layer=0)
+               bloks.append(Block2)
+          if (ph1 or ph2 or card_email or card_www):
+               Block3 = DWIMBLOCK([fx3, CHAIN(ph1, bullet2, ph2), NEWLINE, card_email, NEWLINE, card_www, NEWLINE], (left, 9), (LEFT, BOTTOM), (width2,height2), uniform_linescale=1, layer=0)
+               bloks2.append(Block3)
+          if (card_street1 or card_street2 or card_street3):
+               Block4 = DWIMBLOCK([fx3, card_street1, NEWLINE, card_street2, NEWLINE, card_street3, NEWLINE], (left2, 9), (LEFT, BOTTOM), (width2,height2), uniform_linescale=1, layer=0)
+               bloks2.append(Block4)
+
+          nleft = 0.0
+          newBlockw = 0.0
+          # width = float(width - 9)
+          # width2 = float(width2 - 9)
+          left = 0.0
+          left2 = float(left2-9)
+
+          for block in bloks:
+               newBlockw = float(block._get_right() - block._get_left())
+               if (float(block.lines[0].just) == 0.0):
+                    # Center Align
+                    nleft = float(left - ((newBlockw - width) / 2))
+                    block.move(nleft, 0)
+               elif (float(block.lines[0].just) == -1.0):
+                    # Right Align
+                    nleft = float(left - (newBlockw - width))
+                    block.move(nleft, 0)
+          block = None
+
+          nleft = 0.0
+          newBlockw = 0.0
+          newBlockw = float(Block3._get_right() - Block3._get_left())
+          if (float(Block3.lines[0].just) == 0.0):
+               # Center Align
+               nleft = float(left - ((newBlockw - width2) / 2))
+               Block3.move(nleft, 0)
+          elif (float(Block3.lines[0].just) == -1.0):
+               # Right Align
+               nleft = float(left - (newBlockw - width2))
+               Block3.move(nleft, 0)
+
+          nleft = 0.0
+          newBlockw = 0.0
+          newBlockw = float(Block4._get_right() - Block4._get_left())
+          # COND(str(newBlockw) + ' | ' + str(newBlockw / 72) + ' | ' + str(width2))
+          if (float(Block4.lines[0].just) == 0.0):
+               # Center Align
+               nleft = float((width2 - newBlockw) / 2)
+               Block4.move(nleft, 0)
+          elif (float(Block4.lines[0].just) == -1.0):
+               # Right Align
+               nleft = float(width2 - newBlockw)
+               Block4.move(nleft, 0)
+          # COND(str(float(left2 - (newBlockw - width2))))
+
+
+          # # if (card_street1 or card_street2 or card_street3 or ph1 or ph2 or card_email or card_www):
+          # #      Block3 = DWIMBLOCK([fx3, card_street1, NEWLINE, card_street2, NEWLINE, card_street3, NEWLINE, CHAIN(ph1, bullet2, ph2), NEWLINE, VSPACER(vspc), card_email, NEWLINE, card_www, NEWLINE], (0, 144), (LEFT, TOP), (width,0), uniform_linescale=1, layer=0)
+          # #      bloks.append(Block3)
+          # maxstart = 0
+          # for block in bloks:
+          #      height += (block._get_top()-block._get_bottom()) + vspc
+          #      maxstart = max(maxstart, block._get_top())
+          # block = None
+          # height -= vspc
+          # while (height > 31):
+          #      bloks = []
+          #      # vspc -= 0.1
+          #      bh = 0
+          #      if (card_company or card_department):
+          #           bh = Block1._get_top()-Block1._get_bottom() - 1
+          #           Block1 = DWIMBLOCK([fx, card_company, NEWLINE, card_department, NEWLINE], (0, 144), (LEFT, TOP), (width,bh), layer=0)
+          #           bloks.append(Block1)
+          #      if (full_name or card_title1):
+          #           bh = Block2._get_top()-Block2._get_bottom() - 1
+          #           Block2 = DWIMBLOCK([fx2, full_name, NEWLINE, card_title1, NEWLINE], (0, 144), (LEFT, TOP), (width,bh), layer=0)
+          #           bloks.append(Block2)
+          #      # if (card_street1 or card_street2 or card_street3 or ph1 or ph2 or card_email or card_www):
+          #      #      bh = Block3._get_top()-Block3._get_bottom() - 3
+          #      #      Block3 = DWIMBLOCK([fx3, card_street1, NEWLINE, card_street2, NEWLINE, card_street3, NEWLINE, CHAIN(ph1, bullet2, ph2), NEWLINE, VSPACER(vspc), card_email, NEWLINE, card_www, NEWLINE], (0, 144), (LEFT, TOP), (width,bh), uniform_linescale=1, layer=0)
+          #      #      bloks.append(Block3)
+          #      maxstart = 0
+          #      height = 0
+          #      for block in bloks:
+          #           height += (block._get_top()-block._get_bottom()) + vspc
+          #           maxstart = max(maxstart, block._get_top())
+          #      block = None
+          #      height -= vspc
+          #      if (height <= 31):
+          #           break
+          # # top = float((height - maxstart) / 2)
+          # top = float(-72)
+          # nleft = 0.0
+          # newBlockw = 0.0
+          # for block in bloks:
+          #      newBlockw = float(block._get_right() - block._get_left())
+          #      if (float(block.lines[0].just) == 0.0):
+          #           # Center Align
+          #           nleft = float(left - ((newBlockw - width) / 2))
+          #           block.move(nleft, top)
+          #      elif (float(block.lines[0].just) == -1.0):
+          #           # Right Align
+          #           nleft = float(left - (newBlockw - width))
+          #           block.move(nleft, top)
+          #      else: 
+          #           # Left Align
+          #           block.move(left, top)
+          #      top = -((maxstart - block._get_bottom()) + vspc)
+          # block = None
+
+          # # **************************************************************************************
+          # vspc = 0
+          # if (image):
+          #      left = float(9.0)
+          #      width = float(1.5625*72)
+          #      left2 = float((left*2) + width)
+          # else:
+          #      left = float(9.0)
+          #      width = float(1.5625*72)
+          #      left2 = float((left*2) + width)
+
+          # if (ph1 or ph2 or card_email or card_www):
+          #      Block3 = DWIMBLOCK([fx3, CHAIN(ph1, bullet2, ph2), NEWLINE, card_email, NEWLINE, card_www, NEWLINE], (0, 144), (LEFT, BOTTOM), (width,0), uniform_linescale=1, layer=0)
+          #      bloks.append(Block3)
+          # # if (card_street1 or card_street2 or card_street3):
+          # #      Block4 = DWIMBLOCK([fx3, card_street1, NEWLINE, card_street2, NEWLINE, card_street3, NEWLINE], (0, 144), (LEFT, BOTTOM), (width,0), uniform_linescale=1, layer=0)
+          # #      bloks.append(Block4)
+          # maxstart = 0
+          # for block in bloks:
+          #      height += (block._get_top()-block._get_bottom()) + vspc
+          #      maxstart = max(maxstart, block._get_top())
+          # block = None
+          # height -= vspc
+          # while (height > 31):
+          #      bloks = []
+          #      # vspc -= 0.1
+          #      bh = 0
+          #      if (ph1 or ph2 or card_email or card_www):
+          #           bh = Block3._get_top()-Block3._get_bottom() - 1
+          #           Block3 = DWIMBLOCK([fx3, CHAIN(ph1, bullet2, ph2), NEWLINE, card_email, NEWLINE, card_www, NEWLINE], (0, 144), (LEFT, BOTTOM), (width,bh), uniform_linescale=1, layer=0)
+          #           bloks.append(Block3)
+          #      # if (card_street1 or card_street2 or card_street3):
+          #      #      bh = Block4._get_top()-Block4._get_bottom() - 1
+          #      #      Block4 = DWIMBLOCK([fx3, card_street1, NEWLINE, card_street2, NEWLINE, card_street3, NEWLINE], (0, 144), (LEFT, BOTTOM), (width,bh), uniform_linescale=1, layer=0)
+          #      #      bloks.append(Block4)
+          #      maxstart = 0
+          #      height = 0
+          #      for block in bloks:
+          #           height += (block._get_top()-block._get_bottom()) + vspc
+          #           maxstart = max(maxstart, block._get_top())
+          #      block = None
+          #      height -= vspc
+          #      if (height <= 31):
+          #           break
+          # # top = float((height - maxstart) / 2)
+          # top = float(-135)
+          # nleft = 0.0
+          # newBlockw = 0.0
+          # for block in bloks:
+          #      newBlockw = float(block._get_right() - block._get_left())
+          #      if (float(block.lines[0].just) == 0.0):
+          #           # Center Align
+          #           nleft = float(left - ((newBlockw - width) / 2))
+          #           block.move(nleft, top)
+          #      elif (float(block.lines[0].just) == -1.0):
+          #           # Right Align
+          #           nleft = float(left - (newBlockw - width))
+          #           block.move(nleft, top)
+          #      else: 
+          #           # Left Align
+          #           block.move(left, top)
+          #      top = -((maxstart - block._get_bottom()) + vspc)
+          # block = None
+
+
 except:
      pass
 # xblock = DWIMBLOCK([LEFT, F1, str(left/72), ' - ', str(width/72), ' - ', str(height/72), ' - ', str(top/72), ' - ', str(vspc), NEWLINE], (0, 0), (LEFT, BOTTOM), (3.25*72,0), uniform_linescale=1, layer=0)
