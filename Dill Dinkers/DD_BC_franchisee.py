788122b92e0225e6ce70bc64068cd96e59bb16fd
carberryrob <rcarberry@strategicfactory.com>
2024-07-29 09:59:25 -0400

    Changes to be committed: 	new file:   Dill Dinkers/DD_BC_franchisee.py

---
+++ Dill Dinkers/DD_BC_franchisee.py
@@ -0,0 +1,32 @@
+# Enable logic before blocks section
+
+vspc = 7 #9
+MainText = DWIMLINES([LEFT, F2, name_first, IF(' ', name_last), NEWLINE,
+F4, WRAPON(card_title1, '&', wrap_before =1), NEWLINE,
+NOWRAP, card_title2, NEWLINE,
+card_title3, NEWLINE,
+IFSET(Phone1Word or Phone2Word, VSPACER(vspc)), IFSET(Phone1Word, ['O: ', COL, Phone1Word]), NEWLINE,
+IFSET(Phone2Word, ['D: ', COL, Phone2Word]), NEWLINE,
+IFSET(card_email or LinkedIn, VSPACER(vspc)), card_email, NEWLINE,
+LinkedIn, NEWLINE,
+VSPACER(vspc), F8, 'www.dilldinkers.com', NEWLINE,
+VSPACER(vspc), F9, 'Each franchise independently owned & operated', NEWLINE],[], (1.95*72,1.625*72))
+
+lines = len(MainText)
+
+if (lines >= 12):
+     vspc = 6 #8
+     MainText = DWIMLINES([LEFT, F2, name_first, IF(' ', name_last), NEWLINE,
+     F4, WRAPON(card_title1, '&', wrap_before =1), NEWLINE,
+     NOWRAP, card_title2, NEWLINE,
+     card_title3, NEWLINE,
+     IFSET(Phone1Word or Phone2Word, VSPACER(vspc)), IFSET(Phone1Word, ['O: ', COL, Phone1Word]), NEWLINE,
+     IFSET(Phone2Word, ['D: ', COL, Phone2Word]), NEWLINE,
+     IFSET(card_email or LinkedIn, VSPACER(vspc)), card_email, NEWLINE,
+     LinkedIn, NEWLINE,
+     VSPACER(vspc), F8, 'www.dilldinkers.com', NEWLINE,
+     VSPACER(vspc), F9, 'Each franchise independently owned & operated', NEWLINE],[], (1.95*72,1.625*72))
+
+MainBlock = DWIMBLOCK([MainText], (1.45*72, 1*72), (LEFT, MIDDLE), (1.95*72,1.625*72), layer=0)
+
+
